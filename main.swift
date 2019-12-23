edcb0dc02b72884bc3c25664a6aa4f9e1273e673
Armelle Lecerf <55889292+ArmelleL76@users.noreply.github.com>
2019-12-23 19:55:34 +0100

    Add files via upload
    motif dans le main

--- main.swift
+++ main.swift
@@ -9,9 +9,16 @@
 import Foundation
 
 print("Hello, World!")
-let monRobot = Robot(nomRobot: "Wall-e" , vieRobot: 100, VitesseRobot: 3, positionRobot: (0, 0))
-let monGentilRobot1 = Robot(nomRobot : "Peter", vieRobot: 80, VitesseRobot: 5, positionRobot: (2,3))
-let monGentilRobot2 = Robot(nomRobot: "Alex", vieRobot: 110, VitesseRobot: 4, positionRobot: (-2,-2))
+let monRobot = Robot(nomRobot: "Wall-e", vieRobot: 0, vitesseRobot : 0, positionRobot : (0,0) )
+
+let monGentilRobot1 = Robot(nomRobot : "Peter", vieRobot: 80, vitesseRobot : 5, positionRobot: (2,3))
+monGentilRobot1.vieRobot = 80
+monGentilRobot1.vitesseRobot = 5
+monGentilRobot1.positionRobot = (2,3)
+let monGentilRobot2 = Robot(nomRobot: "Alex", vieRobot: 110, vitesseRobot: 4, positionRobot: (-2,-2))
+monGentilRobot2.vieRobot = 110
+monGentilRobot2.vitesseRobot = 4
+monGentilRobot2.positionRobot = (-2,-2)
 monRobot.sePresenter()
 monGentilRobot1.sePresenter()
 monGentilRobot2.sePresenter()
